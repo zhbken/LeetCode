f1e5c5b558d0f867db2175580bd19f7c16ed2795
Zhassulan <kenzhassulan@gmail.com>
2020-04-30 22:33:57 +0600

    DefangingAnIpAddress1108

---
+++ src/problems/DefangingAnIpAddress1108.java
@@ -0,0 +1,25 @@
+package problems;
+
+/**
+ * Example 1:
+ * Input: address = "1.1.1.1"
+ * Output: "1[.]1[.]1[.]1"
+ *
+ * Example 2:
+ * Input: address = "255.100.50.0"
+ * Output: "255[.]100[.]50[.]0"
+ */
+public class DefangingAnIpAddress1108 {
+
+	public String defangIPaddr(String address) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < address.length(); i++) {
+			if (address.charAt(i) == '.') {
+				builder.append("[.]");
+			} else {
+				builder.append(address.charAt(i));
+			}
+		}
+		return builder.toString();
+	}
+}
